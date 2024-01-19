b6a21af40748780154b52983b440b71256bb32a7
Pete Bell <peter.bell@raspberrypi.org>
2024-01-19 16:28:04 +0000

    fix for hamburger menu

--- en/code/welcome-to-antarctica-complete/responsive-navbar.js
+++ en/code/welcome-to-antarctica-complete/responsive-navbar.js
@@ -1,6 +1,6 @@
 let openHam = document.querySelector('#openHam');
 let closeHam = document.querySelector('#closeHam');
-let navigationItems = document.querySelector('.nav-items')
+let navigationItems = document.getElementById('nav-items')
 
 const hamburgerEvent = (navigation, close, open) => {
     navigationItems.style.display = navigation;
@@ -8,5 +8,16 @@ const hamburgerEvent = (navigation, close, open) => {
     openHam.style.display = open;
 }
 
+const handleResize = () => {
+  const screenWidth = window.innerWidth;
+
+  // Check if the navigation is hidden (display set to "none")
+  if (navigationItems.style.display === "none" && screenWidth > 768) {
+    // If screen width is greater than 768 pixels, show the navigation
+    navigationItems.style.display = "flex";
+  }
+};
+
 openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
 closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
+window.addEventListener("resize", handleResize);
